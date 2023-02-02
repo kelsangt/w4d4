48b7c3e6f2faa139a75a563071add8b43171b479
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-02 17:24:47 -0500

    Complete move method for Towers of Hanoi

--- TDD_projects/lib/towers_of_hanoi.rb
+++ TDD_projects/lib/towers_of_hanoi.rb
@@ -1,5 +1,11 @@
 
 class TowersOfHanoi
-  def initialize()
+  attr_reader :stacks, :move
+  def initialize
+    @stacks = [[1, 2, 3, 4], [], []]
+  end
+  def move(start, ending)
+    @stacks[ending] << @stacks[start].shift
+    return @stacks
   end
 end
--- TDD_projects/spec/towers_of_hanoi_spec.rb
+++ TDD_projects/spec/towers_of_hanoi_spec.rb
@@ -1,13 +1,30 @@
 require "towers_of_hanoi"
 
-describe "#initialize" do
+describe "Towers of Hanoi" do
   subject(:towers_of_hanoi) { TowersOfHanoi.new }
-  it "should create an array" do
-    expect(towers_of_hanoi.stacks).to be_kind_of(Array)
-  end
 
-  it "should contain 3 stacks within the array" do
-    expect(towers_of_hanoi.stacks.length).to eq(3)
+  context "initialize" do 
+    it "should create an array" do
+      expect(towers_of_hanoi.stacks).to be_kind_of(Array)
+    end
+
+    it "should contain 3 stacks within the array" do
+      expect(towers_of_hanoi.stacks.length).to eq(3)
+    end
+    it "should have the first subarray contain all of the discs (4)" do 
+      expect(towers_of_hanoi.stacks[0].length).to eq(4)
+    end
+  end
+  # it "raises an error when the first subarray does not contain all of the discs" do 
+  #   expect { TowersOfHanoi.new }.to raise_error(RunTimeError)
+  # end
+  
+  context "game logic" do 
+    describe "move" do
+      it "should properly move discs to each desired subarray" do 
+        expect(towers_of_hanoi.move(0, 2)).to eq([[2, 3, 4], [], [1]])
+      end
+    end
   end
 
 end
